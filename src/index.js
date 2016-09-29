51fd29f92e1a542d4bb07e284beef17f73412a67
LeeTaeSoon <nrst136@naver.com>
2016-09-29 21:27:50 +0900

    Add method to get recent posts
    Add method to get recent posts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import GetInstaData from './getInstaData';
 
-// parser().then((res) => {
-//   console.log(`all tasks are done! ${res}`);
-// });
-GetInstaData.getTopPosts('트와이스')
-  .then((res) => {
-    console.log(res);
-    console.log('All tasks are done!');
-  });
+/* this is a test code */
+
+// GetInstaData.getTopPosts('트와이스')
+//   .then((res) => {
+//     console.log(res);
+//     console.log('All tasks are done!');
+//   });
+
+// GetInstaData.getRecentPosts('트와이스')
+//   .then((res) => {
+//     console.log(res);
+//     console.log('All tasks are done!');
+//   });
+
+export default GetInstarData;
