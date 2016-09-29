c80f5e288d29a07682b737fc3d03820f794de14c
seungyun cho <cho31250@gmail.com>
2016-09-29 22:24:47 +0900

    Change console output to json file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import GetInstaData from './getInstaData';
+import fs from 'fs';
+import path from 'path';
 
 // parser().then((res) => {
 //   console.log(`all tasks are done! ${res}`);
 // });
 GetInstaData.getTopPosts('트와이스')
   .then((res) => {
-    console.log(res);
+    // console.log(res);
+
+    let file = path.join(__dirname, './', "toppost.json");
+    let json = JSON.stringify(res);
+
+    fs.writeFile(file,json,function(res){
+      console.log(res);
+    });
+
     console.log('All tasks are done!');
   });
