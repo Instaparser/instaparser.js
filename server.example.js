21ec9bd47b8b497771b18d111c22c87cb1bc4d1f
Kenny Oh Dokeun <ohdoking@gmail.com>
2016-09-30 16:11:03 +0900

    instaParser의 예제 소스입니다. (#11)
    * jasmine-node 라이브러리 추가
    
    * insta parser에 예제 코드를 작성했습니다.
    
    아직 css나 빠른 개발을 위해 cdn 사용등 수정이 필요합니다.
    
    * css수정
    
    * 예제를 cdn을 사용하지 않고 node로 모듈을 다운받아서 고정된 lib로 사용하도록 변경하였습니다.
    
    * config.json에 있는 파일의 tag명으로 검색하여 화면에 보여지도록 만들었습니다.
    
    config.json 에 tag 명을 수정하시면 해당 tag명으로 검색이되고 example 화면에 보여지게됩니다.
    
    * tag 명 수정 테스트함

---
+++ server.example.js
@@ -0,0 +1,39 @@
+/*
+  예제를 돌리기위한 node server 설정입니다.
+  express를 사용하였습니다.
+ */
+
+var express = require('express');
+var app = express();
+var fs = require('fs');
+var config = require('./config.json');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/example');
+
+app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
+app.use('/slick', express.static(__dirname + '/node_modules/slick-carousel/slick/'));
+
+
+app.get('/', function(req, res) {
+  var tag = config.tag;
+  var topPost;
+  var recentPost;
+  fs.readFile('./example/toppost.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    topPost = JSON.parse(data);
+    fs.readFile('./example/mostpost.json', 'utf8', function (err, data) {
+      if (err) throw err;
+      recentPost = JSON.parse(data);
+      res.render('example',{
+        "tag": tag,
+        "topPost": topPost,
+        "recentPost": recentPost
+      });
+    });
+  });
+});
+
+
+app.listen(8081);
+console.log('8081 connect');
