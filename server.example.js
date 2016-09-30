1d8437c1e3827103b69924d9bfea0d4e42884d99
Tylor Shin <shincode@icloud.com>
2016-09-30 21:15:27 +0900

    Rearranged server example file

--- server.example.js
+++ server.example.js
@@ -1,36 +1,35 @@
-/*
-  예제를 돌리기위한 node server 설정입니다.
-  express를 사용하였습니다.
- */
+'use strict';
+/* **********************************************************************
+ExpreeJS server to run example code
+*********************************************************************** */
+const express = require('express');
 
-var express = require('express');
-var app = express();
-var fs = require('fs');
+const app = express();
+const fs = require('fs');
 
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/example');
+app.set('views', `${__dirname}/example`);
 
-app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
-app.use('/slick', express.static(__dirname + '/node_modules/slick-carousel/slick/'));
-app.use('/js', express.static(__dirname + '/example/js/'));
-app.use('/css', express.static(__dirname + '/example/css/'));
+app.use('/jquery', express.static(`${__dirname}/node_modules/jquery/dist/`));
+app.use('/slick', express.static(`${__dirname}/node_modules/slick-carousel/slick/`));
+app.use('/js', express.static(`${__dirname}/example/js/`));
+app.use('/css', express.static(`${__dirname}/example/css/`));
 
-
-
-app.get('/', function(req, res) {
+app.get('/', (req, res) => {
   const tag = process.env.TAG || 'hayeonsoo';
-  var topPost;
-  var recentPost;
-  fs.readFile('./example/toppost.json', 'utf8', function (err, data) {
+  let topPost;
+  let recentPost;
+  fs.readFile('./example/toppost.json', 'utf8', (err, data) => {
+
     if (err) throw err;
     topPost = JSON.parse(data);
-    fs.readFile('./example/mostpost.json', 'utf8', function (err, data) {
+    fs.readFile('./example/mostpost.json', 'utf8', (error, result) => {
       if (err) throw err;
-      recentPost = JSON.parse(data);
-      res.render('example',{
-        "tag": tag,
-        "topPost": topPost,
-        "recentPost": recentPost
+      recentPost = JSON.parse(result);
+      res.render('example', {
+        tag,
+        topPost,
+        recentPost,
       });
     });
   });
@@ -38,4 +37,5 @@ app.get('/', function(req, res) {
 
 
 app.listen(8081);
-console.log('8081 connect');
+
+console.log('⚡️Example server is running on http://localhost:8081');
