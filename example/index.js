ebf130ad9572d214fd0b2705ac88ad0a37f51371
Tylor Shin <shincode@icloud.com>
2016-09-30 19:37:08 +0900

    Rearragned javascript syntax

--- example/index.js
+++ example/index.js
@@ -1,33 +1,30 @@
-import GetInstaData from '../src/getInstaData';
 import fs from 'fs';
 import path from 'path';
+import GetInstaData from '../src/getInstaData';
 import config from '../config.json';
 
 /* this is a example code */
 
 GetInstaData.getTopPosts(config.tag)
- .then((res) => {
+  .then((res) => {
+    const file = path.join(__dirname, './', 'toppost.json');
+    const json = JSON.stringify(res);
 
-    let file = path.join(__dirname, './', "toppost.json");
-    let json = JSON.stringify(res);
-
-    fs.writeFile(file,json,function(res){
-      // console.log(json);
+    fs.writeFile(file, json, (result) => {
+      console.log(result);
+      console.log('Toppost : All tasks are done!');
     });
-    console.log('Toppost : All tasks are done!');
-   });
+  });
 
 GetInstaData.getRecentPosts(config.tag)
   .then((res) => {
-    let file = path.join(__dirname, './', "mostpost.json");
-    let json = JSON.stringify(res);
+    const file = path.join(__dirname, './', 'mostpost.json');
+    const json = JSON.stringify(res);
 
-    fs.writeFile(file,json,function(res){
-      // console.log(json);
+    fs.writeFile(file, json, (result) => {
+      console.log(result);
+      console.log('Mostpost : All tasks are done!');
     });
-
-    console.log('Mostpost : All tasks are done!');
-
   });
 
 export default GetInstaData;
